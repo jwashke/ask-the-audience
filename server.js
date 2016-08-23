319892a80906522746c630e41b7014b2783f38f4
Josh Washke <jwashke@icloud.com>
2016-08-22 19:24:13 -0600

    Add buttons for voting to page, add votes hash in server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const express = require('express');
+var votes = {};
 
 const app = express();
 
@@ -23,6 +24,15 @@ io.on('connection', function(socket) {
   console.log('A user has connected.', io.engine.clientsCount);
 
   io.sockets.emit('usersConnected', io.engine.clientsCount);
+
+  socket.emit('statusMessage', 'You have connected.');
+
+  socket.on('message', function(channel, message) {
+    if (channel === 'voteCast') {
+      votes[socket.id] = message;
+      console.log(votes);
+    }
+  });
 
   socket.on('disconnect', function() {
     console.log('A user has disconnected.', io.engine.clientsCount);
